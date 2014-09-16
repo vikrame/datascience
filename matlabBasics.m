6360139a11fc082afa00e9d4a883fc74d8f64dcb
Vikram Elango <vikrame@gmail.com>
2014-09-16 19:58:10 -0400

    Create matlabBasics.m

---
+++ matlabBasics.m
@@ -0,0 +1,58 @@
+%row matrix
+a = [1 2 3 4]
+
+% 3 X # Matrix
+b = [ 1 2 3; 4 5 6; 7 8 9]
+
+% Column vector with 0's
+z = zeros(5,1)
+
+% Matrix Addition
+c = b + 10
+
+%Sin of elements
+d = sin(b)
+
+%Matrix Transpose
+t = b'
+
+% Matrix time its Inverse
+p = b * inv(b)
+
+%Element wise Multiplication
+q = b.*b
+cube= b.^3
+
+%Concatenation
+A = [b,b]
+
+B = [b;b]
+
+%complex Numbers
+sqrt(-1)
+
+%Array Indexing
+M = magic(4)
+
+M(4,2)
+
+M(8)
+
+M(4,5) = 17
+
+M(1:3, 4)
+
+M(1:3,:)
+
+B = 0:10:100
+
+A = magic(4);
+
+B = rand(3,5,2);
+
+
+
+
+
+
+
