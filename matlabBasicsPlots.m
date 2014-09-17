0792297c80a0be6647287132e4a8fecbf3448e5e
Vikram Elango <vikrame@gmail.com>
2014-09-16 20:29:19 -0400

    Create matlabBasicsPlots.m

---
+++ matlabBasicsPlots.m
@@ -0,0 +1,43 @@
+%Line Plots
+x = 0:pi/100:2*pi;
+y = sin(x);
+plot(x,y)
+
+%Red Dashed Lines
+plot(x,y,'r--')
+
+xlabel('x')
+ylabel('sin(x)')
+title('Plot of the Sine Function')
+
+%Hold On
+x = 0:pi/100:2*pi;
+y = sin(x);
+plot(x,y)
+
+hold on
+
+y2 = cos(x); 
+plot(x,y2,'r:') 
+legend('sin','cos')
+
+%3D
+[X,Y] = meshgrid(-2:.2:2)
+Z = X .* exp(-X.^2 - Y.^2)
+surf(X,Y,Z)
+
+%four plots in a 2-by-2 grid
+t = 0:pi/10:2*pi;
+[X,Y,Z] = cylinder(4*cos(t)); 
+subplot(2,2,1); 
+mesh(X); 
+title('X'); 
+subplot(2,2,2); 
+mesh(Y);
+title('Y'); 
+subplot(2,2,3); 
+mesh(Z); 
+title('Z'); 
+subplot(2,2,4); 
+mesh(X,Y,Z); 
+title('X,Y,Z');
